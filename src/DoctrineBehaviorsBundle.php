995fa530d59ca82e0725a6140f87af6d37d9b16f
mathieu <mathieu.lagarde@sccd.lu>
2024-03-06 13:32:00 +0100

    fix: Bundle class definition

--- src/DoctrineBehaviorsBundle.php
+++ src/DoctrineBehaviorsBundle.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace Knp\DoctrineBehaviors;
 
 use Knp\DoctrineBehaviors\Bundle\DependencyInjection\DoctrineBehaviorsExtension;
-use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DoctrineBehaviorsBundle extends Bundle
 {
-    public function getContainerExtension(): Extension
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new DoctrineBehaviorsExtension();
     }
